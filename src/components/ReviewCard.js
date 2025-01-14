084ad4632edac4a6f6bbb1f86e80614e2ab1d5a9
Miguel Castro <mcastro32902@gmail.com>
2025-01-14 18:20:54 -0500

    Update ReviewCard.js

--- src/components/ReviewCard.js
+++ src/components/ReviewCard.js
@@ -4,7 +4,7 @@ const ReviewCard = ({ name, review, image }) => {
   return (
     <div className="bg-white p-8 rounded-xl shadow-lg hover:shadow-2xl transition-all duration-300 ease-in-out">
       <div className="text-6xl text-primary text-left" style={{ fontFamily: 'Pattaya, sans-serif' }}>“</div>
-      <p className="italic text-grey-600 text-left">"{review}"</p>
+      <p className="italic text-grey-600 text-left">{review}</p>
       <div className="text-6xl text-primary text-right" style={{ fontFamily: 'Pattaya, sans-serif' }}>”</div>
       <div className="flex items-center">
         <img src={image} alt={name} className="w-12 h-12 rounded-full mr-4 object-cover object-center" />
